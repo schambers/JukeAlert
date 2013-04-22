38ad881f927fa6aded790669a4cc04c2494e5ecb
Mark Grandi <markgrandi@gmail.com>
2013-04-22 12:53:28 -0700

    added comments, prevented the JukeAlertLogger from being deallocated
    made it so that we have an instance variable for the logger, cause we
    were creating it but then it would be deallocated after onEnable
    finished. Added comments too

--- src/com/untamedears/JukeAlert/JukeAlert.java
+++ src/com/untamedears/JukeAlert/JukeAlert.java
@@ -12,17 +12,20 @@ public class JukeAlert extends JavaPlugin {
 	private Manager manager;
 	public static final Logger LOGGER = Logger.getLogger("Minecraft");
 	private JukeAlertLogger jaLogger;
+	private JukeAlertCommands jaCommandExecutor;
 	private List<JukeAlertSnitch> snitches = new ArrayList<>(); //TODO: Add snitches to memory so it's not server intensive going to the SQL everytime.
 	
 	@Override
 	public void onEnable() {
-		manager.load();
-		jaLogger = new JukeAlertLogger(this);
+		this.manager.load();
+		this.jaLogger = new JukeAlertLogger(this);
 
-		JukeAlertCommands commands = new JukeAlertCommands(this);
+		// create our class that executes commands, and then set it 
+		// to be the CommandExecutor for all of our commands defined in our plugin.yaml
+		this.jaCommandExecutor = new JukeAlertCommands(this);
 		for (String command : getDescription().getCommands().keySet()) {
 
-			getCommand(command).setExecutor(commands);
+			getCommand(command).setExecutor(jaCommandExecutor);
 		}
 	
 	}
