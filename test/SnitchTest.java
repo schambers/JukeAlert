8ca010dd2f69177b9024137bcd67627f8cc4b9e6
Sean Chambers <schambers80@gmail.com>
2013-05-26 12:03:08 -0400

    wiring up test functionality to ant and have one basic test to ensure everything works correctly

--- test/SnitchTest.java
+++ test/SnitchTest.java
@@ -1,6 +1,19 @@
-import junit.framework.Test;
-import junit.framework.TestCase;
+import com.untamedears.JukeAlert.model.Snitch;
+import com.untamedears.citadel.entity.Faction;
+import org.bukkit.Location;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-public class SnitchTest extends TestCase {
+public class SnitchTest {
+
+    @Test
+    public void snitch_starts_with_empty_name(){
+        Location location = Mockito.mock(Location.class);
+        Faction group = Mockito.mock(Faction.class);
+
+        Snitch snitch = new Snitch(location, group);
+        assert snitch.getName().equals("");
+    }
+
 
 }
